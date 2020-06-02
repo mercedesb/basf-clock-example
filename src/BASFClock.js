f42a04c2e25bbbc673b046358fd26724efb63143
Mercedes Bernard <mercedesrbernard@gmail.com>
2020-06-02 08:45:13 -0500

    Use jquery for clock

--- src/BASFClock.js
+++ src/BASFClock.js
@@ -1,26 +1,5 @@
-import React, { useEffect, useRef } from "react";
+import React from "react";
 
 export function BASFClock(props) {
-  const clockRef = useRef(null);
-
-  useEffect(() => {
-    fetch(
-      "https://d1iq3ppa2tzmm4.cloudfront.net/basf-countdown-clock-source.html"
-    )
-      .then((response) => response.text())
-      .then((html) => {
-        clockRef.current.innerHTML = html;
-      });
-  });
-
-  return (
-    <React.Fragment>
-      <link
-        rel="stylesheet"
-        type="text/css"
-        href="//d1iq3ppa2tzmm4.cloudfront.net/basf-countdown-clock-source.css"
-      />
-      <div id="countdown-clock-widget-618815" ref={clockRef}></div>
-    </React.Fragment>
-  );
+  return <div id="countdown-clock-widget-618815"></div>;
 }
---
+++ src/BASFClockNoJQuery.js
@@ -0,0 +1,26 @@
+import React, { useEffect, useRef } from "react";
+
+export function BASFClockNoJQuery(props) {
+  const clockRef = useRef(null);
+
+  useEffect(() => {
+    fetch(
+      "https://d1iq3ppa2tzmm4.cloudfront.net/basf-countdown-clock-source.html"
+    )
+      .then((response) => response.text())
+      .then((html) => {
+        clockRef.current.innerHTML = html;
+      });
+  });
+
+  return (
+    <React.Fragment>
+      <link
+        rel="stylesheet"
+        type="text/css"
+        href="//d1iq3ppa2tzmm4.cloudfront.net/basf-countdown-clock-source.css"
+      />
+      <div id="countdown-clock-widget-618815" ref={clockRef}></div>
+    </React.Fragment>
+  );
+}
