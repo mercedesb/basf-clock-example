e5596d7677df9f335f88c200762adf0f5aae08e1
Mercedes Bernard <mercedesrbernard@gmail.com>
2020-08-03 14:22:27 -0500

    another change for release drafter testing

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BASFClock } from "./BASFClock";
 
 // making some changes to test release drafter
+// and another change
 
 function App() {
   return <BASFClock />;
